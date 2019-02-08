ca6874967e6a8e6c768d974fc694cd9cd261d8c5
gurmukh_sahota@hotmail.co.uk <gurmukh_sahota@hotmail.co.uk>
2019-02-08 12:10:05 +0000

    Updates

---
+++ precache-manifest.a74fce29e9ec568729cb7aae1811ba8e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a4b7f937ebee724d05",
+    "url": "/Gurmukh-sahota.github.io/static/js/runtime~main.d7a4b7f9.js"
+  },
+  {
+    "revision": "e24c8c8cde8c79c07a97",
+    "url": "/Gurmukh-sahota.github.io/static/js/main.e24c8c8c.chunk.js"
+  },
+  {
+    "revision": "0963e9e8ddace2f9b5a9",
+    "url": "/Gurmukh-sahota.github.io/static/js/1.0963e9e8.chunk.js"
+  },
+  {
+    "revision": "e24c8c8cde8c79c07a97",
+    "url": "/Gurmukh-sahota.github.io/static/css/main.310aa65d.chunk.css"
+  },
+  {
+    "revision": "2ad61b8af03fb724aea9cd42b7b49fbd",
+    "url": "/Gurmukh-sahota.github.io/index.html"
+  }
+];
